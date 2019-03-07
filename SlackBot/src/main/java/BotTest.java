09fc1bab34f24ca31cdd47a4befa41b62b0a9238
jacoloves <5511068t@gmail.com>
2019-03-07 23:57:28 +0900

    practice

---
+++ SlackBot/src/main/java/BotTest.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class BotTest {
+    public static void main(String[] args) throws IOException {
+        String botToken = "xoxb-391697228628-568684645156-yn0TQXsiXUaPfHbIbKSlreZi";
+
+        SlackletService
+    }
+}
